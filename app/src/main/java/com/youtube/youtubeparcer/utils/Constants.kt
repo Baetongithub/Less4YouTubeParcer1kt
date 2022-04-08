96d903d2d0ca59d3ef19c57adba927f23201a906
Baet <toktonbekovbaet@gmail.com>
2022-04-09 03:43:07 +0600

    some color changes

--- app/src/main/java/com/youtube/youtubeparcer/utils/Constants.kt
+++ app/src/main/java/com/youtube/youtubeparcer/utils/Constants.kt
@@ -3,7 +3,7 @@ package com.youtube.youtubeparcer.utils
 object Constants {
 
     const val PART = "snippet,contentDetails"
-    const val CHANNEL_ID = "UC1HJy3m76-yikrSMfljk21Q"
+    const val CHANNEL_ID = "UC0WP5P-ufpRfjbNrmOWwLBQ"
 
     const val ITEMS_DETAILED = "items_serializable"
     const val ITEMS_PLAYLIST = "items_playlist"
