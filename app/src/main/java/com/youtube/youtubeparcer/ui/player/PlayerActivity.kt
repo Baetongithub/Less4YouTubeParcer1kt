ca0a8478e1fd0d1277a8afbf3fbd29faad462428
Baet <toktonbekovbaet@gmail.com>
2022-04-25 16:12:51 +0600

    now the "Download" works (not quite cool, will be improved)

--- app/src/main/java/com/youtube/youtubeparcer/ui/player/PlayerActivity.kt
+++ app/src/main/java/com/youtube/youtubeparcer/ui/player/PlayerActivity.kt
@@ -1,6 +1,10 @@
 package com.youtube.youtubeparcer.ui.player
 
 import android.annotation.SuppressLint
+import android.app.DownloadManager
+import android.content.Context
+import android.net.Uri
+import android.os.Environment
 import android.util.SparseArray
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
@@ -8,11 +12,6 @@ import androidx.core.view.WindowInsetsControllerCompat
 import at.huber.youtubeExtractor.VideoMeta
 import at.huber.youtubeExtractor.YouTubeExtractor
 import at.huber.youtubeExtractor.YtFile
-import com.youtube.youtubeparcer.databinding.ActivityPlayerBinding
-import com.youtube.youtubeparcer.utils.Constants
-import com.youtube.youtubeparcer.base.BaseActivity
-import com.youtube.youtubeparcer.extensions.toast
-import com.youtube.youtubeparcer.data.model.playlistItems.Items
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.source.MediaSource
@@ -21,6 +20,12 @@ import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.ui.StyledPlayerView
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
 import com.google.android.exoplayer2.util.Util
+import com.youtube.youtubeparcer.R
+import com.youtube.youtubeparcer.base.BaseActivity
+import com.youtube.youtubeparcer.data.model.playlistItems.Items
+import com.youtube.youtubeparcer.databinding.ActivityPlayerBinding
+import com.youtube.youtubeparcer.extensions.toast
+import com.youtube.youtubeparcer.utils.Constants
 
 class PlayerActivity : BaseActivity<ActivityPlayerBinding>(ActivityPlayerBinding::inflate) {
 
@@ -33,12 +38,48 @@ class PlayerActivity : BaseActivity<ActivityPlayerBinding>(ActivityPlayerBinding
     override fun setUpUI() {
         val items = intent.getSerializableExtra(Constants.ITEMS_DETAILED) as Items
 
+        val videoID = items.contentDetails?.videoId
+        val url = Constants.YT_WATCH_LINK + videoID
+
         vb.tvPlayerTitle.text = items.snippet?.title
         vb.tvPlayerDescription.text = items.snippet?.description
 
         vb.tvPlayerBack.setOnClickListener { finish() }
 
-        vb.tvPlayerDownload.setOnClickListener { toast(vb.tvPlayerDownload.text.toString()) }
+        vb.tvBtnDownload.setOnClickListener {
+
+            object : YouTubeExtractor(this) {
+                @SuppressLint("ServiceCast")
+                override fun onExtractionComplete(
+                    ytFiles: SparseArray<YtFile>?,
+                    videoMeta: VideoMeta?
+                ) {
+                    if (ytFiles != null) {
+                        val tag = 22
+                        val downloadUrl = ytFiles[tag].url
+                        val downloadManager: DownloadManager =
+                            getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+                        val request: DownloadManager.Request =
+                            DownloadManager.Request(
+                                Uri.parse(downloadUrl)
+                            )
+                        request.apply {
+                            setTitle(videoMeta?.title)
+                            setDescription(videoMeta?.shortDescription)
+                            setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                            setDestinationInExternalPublicDir(
+                                Environment.DIRECTORY_DOWNLOADS,
+                                "Downloads"
+                            )
+                        }
+                        downloadManager.enqueue(request)
+                    }
+                }
+
+            }.extract(url)
+
+            toast(getString(R.string.downloading))
+        }
 
         initPlayer()
     }
@@ -50,7 +91,7 @@ class PlayerActivity : BaseActivity<ActivityPlayerBinding>(ActivityPlayerBinding
         val items = intent.getSerializableExtra(Constants.ITEMS_DETAILED) as Items
 
         val videoID = items.contentDetails?.videoId
-        val url = "https://www.youtube.com/watch?v=${videoID}"
+        val url = Constants.YT_WATCH_LINK + videoID
 
         playVideo(url)
     }
--- app/src/main/java/com/youtube/youtubeparcer/utils/Constants.kt
+++ app/src/main/java/com/youtube/youtubeparcer/utils/Constants.kt
@@ -3,9 +3,10 @@ package com.youtube.youtubeparcer.utils
 object Constants {
 
     const val PART = "snippet,contentDetails"
-    const val CHANNEL_ID = "UC0WP5P-ufpRfjbNrmOWwLBQ"
+    const val CHANNEL_ID = "UC1HJy3m76-yikrSMfljk21Q"
 
     const val ITEMS_DETAILED = "items_serializable"
     const val ITEMS_PLAYLIST = "items_playlist"
 
+    const val YT_WATCH_LINK = "https://www.youtube.com/watch?v="
 }
